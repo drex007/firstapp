184ac37987738aaac6d24b13ffa1e5a5a71db0c3
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-01-31 20:03:12 +0000

    Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -20,45 +20,61 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("TrevInvest"),
       ),
-      body: Container(
-        width: 700,
-        color: Colors.grey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.grey,
+          
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
           children: [
-            Container(
-         padding: const EdgeInsets.all(8),
-   
-         width: 100,
-         height: 100,
-         alignment: Alignment.center,
-         color:  Colors.pink,
-         
-         
-        ),
-        Container(
-         padding: const EdgeInsets.all(8),
-   
-         width: 100,
-         height: 100,
-         alignment: Alignment.center,
-         color:  Colors.green,
-         
-         
-        ),
-        Container(
-         padding: const EdgeInsets.all(8),
-   
-         width: 100,
-         height: 100,
-         alignment: Alignment.center,
-         color:  Colors.purple,
-         
-         
-        ),
+            UserAccountsDrawerHeader(accountName: Text("Anonymous User"),
+             accountEmail: Text("Anonymous@gmail.com"),
+             currentAccountPicture:CircleAvatar(backgroundImage: NetworkImage("") ),
+             ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Anonymous User"),
+              trailing: Icon(Icons.edit),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.money ),
+              title: Text("Account Balance"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.mail_outline ),
+              title: Text("Fund Account"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.folder),
+              title: Text("Request Withdrawal"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: (){},
+            ),
+            ListTile(
+              leading: Icon(Icons.mail),
+              title: Text("Anonymous User"),
+              subtitle: Text("anonymous@gmail.com"),
+              trailing: Icon(Icons.h_mobiledata),
+              onTap: (){},
+            ),
+
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: null,
+      child: Icon(Icons.edit),
+
+
       ),
     );
   }
