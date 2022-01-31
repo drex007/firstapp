ad46e4e42b58f5080ad6575a59939fefede5c54e
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-01-31 16:41:55 +0000

    text properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
@@ -16,10 +18,32 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("FirstApp"),
+        title: Text("TrevInvest"),
       ),
-      body: Container(
-        child: Text("Hi buddy"),
+      body: Center(
+        child: Container(
+         padding: const EdgeInsets.all(8),
+   
+         width: 350,
+         height: 100,
+         alignment: Alignment.center,
+         decoration: BoxDecoration(
+           color: Colors.pink,
+           borderRadius: BorderRadius.circular(10),
+           gradient: LinearGradient(colors: [
+             Colors.pink,
+             Colors.black,
+           ]),
+           
+         ),
+         child: Text("I am a box",
+         textAlign: TextAlign.center,
+         style: TextStyle(
+           fontSize: 30,
+           color: Colors.white,
+           fontWeight: FontWeight.bold,
+         )),
+        ),
       ),
     );
   }
